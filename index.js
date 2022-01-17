3cc61bc8b5e91f3d8832764c71d6b9fe9d79ace5
Mohamed Muflahi <muflahimohamed18@gmail.com>
2022-01-17 12:07:18 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const element = document.querySelector('#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Mohamed is the champion";
